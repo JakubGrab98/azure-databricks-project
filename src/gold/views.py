a9d1b6d3fb8af266c5bfef3395099246d3ca407f
Jakub Grabarczyk <kubagrabarczyk98@gmail.com>
2025-03-26 20:19:04 +0100

    added dim_prices

--- src/gold/views.py
+++ src/gold/views.py
@@ -6,8 +6,23 @@ def create_dim_players(spark: SparkSession):
     CREATE OR REPLACE VIEW gold.dim_players AS
     SELECT 
         ROW_NUMBER() OVER (ORDER BY unique_playerid) AS player_key,
+        playerid,
         nickname,
         country,
         source_folder AS console
     FROM silver.players
+    """)
+
+def create_dim_prices(spark: SparkSession):
+    spark.sql("""
+    CREATE OR REPLACE VIEW gold.dim_prices AS
+    SELECT 
+        ROW_NUMBER() OVER (ORDER BY unique_gameid) AS player_key,
+        gameid,
+        source_folder AS console,
+        price,
+        currency,
+        start_date,
+        end_date
+    FROM silver.games_prices
     """)
